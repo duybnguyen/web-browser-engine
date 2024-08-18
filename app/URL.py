dced3ca1ff5bddd7021045e1bf50ceeca7b5f83c
Duy-B-Nguyen <duybnguyen04@gmail.com>
2024-08-18 03:32:29 -0400

    add show function which prints body of response and load function to parse url

--- app/URL.py
+++ app/URL.py
@@ -8,44 +8,41 @@ class URL:
       self.path = "/" + url
 
 
-      def request(self):
-         # create a socket
-         s = socket.socket(
-            family = socket.AF_INET,
-            type = socket.SOCK_STREAM,
-            proto = socket.IPPROTO_TCP
-         )
+   def request(self):
+      # create a socket
+      s = socket.socket(
+         family = socket.AF_INET,
+         type = socket.SOCK_STREAM,
+         proto = socket.IPPROTO_TCP
+      )
 
-         # connect to the host using the socket on port 80
-         s.connect((self.host, 80))
+      # connect to the host using the socket on port 80
+      s.connect((self.host, 80))
 
-         # form a request
-         request = "GET {} HTTP/1.0\r\n".format(self.path)
-         request += "Host: {}\r\n".format(self.host)
-         request += "\r\n" #signals end of request
-         s.send(request.encode("utf8"))
+      # form a request
+      request = "GET {} HTTP/1.0\r\n".format(self.path)
+      request += "Host: {}\r\n".format(self.host)
+      request += "\r\n" #signals end of request
+      s.send(request.encode("utf8"))
 
-         # reading the server's response
-         response = s.makefile("r", encoding="utf", newline="\r\n")
-         # reading the status line
-         statusline = response.readline()
-         version, status, explanation = statusline.split(" ", 2)
-         # reading the headers
-         response_headers = {}
-         while True:
-            line = response.readline()
-            if line == "\n\r": break # if the line is just an empty line that means it's the end of the response
-            header, value = line.split(":", 1)
-            response_headers[header.casefold()] = value.strip()
-         
-         # can't process these headers yet
-         assert "transfer-encoding" not in response_headers
-         assert "content-encoding" not in response_headers
-
-         # read the rest of the response which is the body
-         content = response.read()
-         s.close()
-         return content
-
-
+      # reading the server's response
+      response = s.makefile("r", encoding="utf", newline="\r\n")
+      # reading the status line
+      statusline = response.readline()
+      version, status, explanation = statusline.split(" ", 2)
+      # reading the headers
+      response_headers = {}
+      while True:
+         line = response.readline()
+         if line == "\r\n": break # if the line is just an empty line that means it's the end of the response
+         header, value = line.split(":", 1)
+         response_headers[header.casefold()] = value.strip()
       
+      # can't process these headers yet
+      assert "transfer-encoding" not in response_headers
+      assert "content-encoding" not in response_headers
+
+      # read the rest of the response which is the body
+      content = response.read()
+      s.close()
+      return content
---
+++ app/main.py
@@ -0,0 +1,21 @@
+from URL import URL
+
+# prints the body of the response skipping over html tags
+def show(body):
+   in_tag = False
+   for c in body:
+      if c == "<":
+         in_tag = True
+      if c == ">":
+         in_tag = False
+      elif not in_tag:
+         print(c, end="")
+
+def load(url):
+   body = url.request()
+   show(body)
+
+
+if __name__ == "__main__":
+   import sys
+   load(URL(sys.argv[1]))
