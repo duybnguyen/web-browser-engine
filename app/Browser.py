7ff82b1cc5497eebec46f1c9f7c525e76b877417
Duy-B-Nguyen <duybnguyen04@gmail.com>
2024-08-19 16:00:51 -0400

    add new events for scrolling

--- app/Browser.py
+++ app/Browser.py
@@ -1,7 +1,7 @@
 import tkinter
-from utils.defs import CANVAS_WIDTH, CANVAS_HEIGHT, SCROLL_STEP, layout
+from utils.defs import CANVAS_WIDTH, CANVAS_HEIGHT, SCROLL_STEP
 from app.URL import URL
-from utils.helpers import lex
+from utils.helpers import lex, layout
 
 class Browser: 
    def __init__(self):
@@ -13,23 +13,26 @@ class Browser:
          height = CANVAS_HEIGHT
       )
       self.canvas.pack()
+      self.display_list = []
       self.scroll = 0
       
-      self.window.bind("<Down>", self.scrolldown)
+      self.window.bind("<MouseWheel>", self.scroll_down)
+      self.window.bind("<KeyPress-Down>", self.scroll_down)
 
    def draw(self):
       self.canvas.delete("all")
       for x, y, c in self.display_list:
-         self.canvas_create_text(x, y - self.scroll , text = c)
+         self.canvas.create_text(x, y - self.scroll, text = c)
 
    # write body to canvas
    def load(self, url):
-      parsedURL = URL(url).request()
+      parsedURL = URL(url)
+      parsedURL = parsedURL.request()
       body = lex(parsedURL)
       self.display_list = layout(body)
       self.draw()
 
-   def scrollDown(self, e):
+   def scroll_down(self, e):
       self.scroll += SCROLL_STEP
       self.draw()
    
